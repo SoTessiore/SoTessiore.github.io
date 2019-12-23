5f149c9bcd74e1a860b038cdbc3b868081e911e8
SoTessiore <59150692+SoTessiore@users.noreply.github.com>
2019-12-23 15:39:05 +0100

    Add files via upload

---
+++ envoi.php
@@ -0,0 +1,19 @@
+<?php
+
+/*on récupère les données du formulaire*/
+
+$nom = $_POST['nom'] ;
+$email = $_POST['email'] ;
+$message = $_POST['message'] ;
+
+/*on les envoie avec la fonction mail()*/
+
+$retour = mail('solene.tessiore@gmail.com', 'Envoi d\'un formulare', "Nom: <b>".$nom."</b><br>\nEmail: <b>".$email."</b><br>\nMessage: <b>".$message."</b><br>\n");
+if ($retour) {
+    echo '<p>Votre message a bien été envoyé.</p>';
+}
+else {
+    echo '<p>Erreur lors de l\'envoi<p>';
+}
+
+?>
